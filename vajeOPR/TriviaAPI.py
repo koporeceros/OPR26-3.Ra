732da0c45ce37b9e77385dd3aa7e3f39b93e6a56
koporeceros <86314117+koporeceros@users.noreply.github.com>
2026-09-22 10:55:00 +0000

    TrvAPI

--- vajeOPR/TriviaAPI.py
+++ vajeOPR/TriviaAPI.py
@@ -1,5 +1,7 @@
 import requests
 import pprint
+import random
+
 urlTrivia = "https://opentdb.com/api.php?amount=10&type=multiple"
 
 
@@ -7,5 +9,33 @@ kt = requests.get(urlTrivia).json()
 
 rez = kt["results"]
 
-print(len(rez))
-pprint.pprint(kt["results"][0])
+#print(len(rez))
+#pprint.pprint(kt["results"][0])
+#print(rez)
+"""
+for r in rez:
+    print("-" * 80)
+    print(r["question"])
+    print(r["correct_answer"])
+    pprint.pprint(r["incorrect_answers"])
+"""
+total = 0
+
+for r in rez:
+    print("-" * 80)
+    print(r["question"])
+    cor = r["correct_answer"]
+    together = r["incorrect_answers"]
+    together.append(cor)
+    print(random.shuffle(together))
+    
+    odgovor = input("Vnesi odgovor: ")
+    if odgovor == cor:
+        print("Pravilno")
+        total += 1
+    else:
+        print("narobe")
+
+print(f"Pravilno ste odgovorina na {total}/10 vprašanj")
+
+
